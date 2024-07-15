e143a2ca5db0515abf96ae1033bc0e00e61e12ae
creinis <115181527+creinis@users.noreply.github.com>
2024-07-15 19:36:29 +0200

    create: sequence customer id

--- bikes.sql
+++ bikes.sql
@@ -97,6 +97,17 @@ ALTER TABLE public.customers OWNER TO freecodecamp;
 -- Name: customers_customer_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
 --
 
+CREATE SEQUENCE public.customers_customer_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.customers_customer_id_seq OWNER TO freecodecamp;
+
 --
 -- Name: customers_customer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
 --
