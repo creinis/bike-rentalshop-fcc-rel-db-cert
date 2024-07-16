7b29e967f034f5b5f374705fe03026e992226380
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 08:14:42 +0200

    add: intro, main menu options, read menu

--- bike-shop.sh
+++ bike-shop.sh
@@ -5,6 +5,13 @@ PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 echo -e "\n~~~~~~~~~~ Bike Rental Shop ~~~~~~~~~~\n"
 
 MAIN_MENU() {
+
+    echo "How may I help you?" 
+    echo -e "\n
+        1. Rent a bike\n
+        2. Return a bike\n
+        3. Exit"
+    read MAIN_MENU_SELECTION
 }
 
 RETURN_MENU() {
