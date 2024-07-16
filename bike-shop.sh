d0cc0f8a04a4fd4035dbb947fb456a659ba17701
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 09:08:33 +0200

    feat: insert bike rental

--- bike-shop.sh
+++ bike-shop.sh
@@ -77,6 +77,10 @@ RENT_MENU() {
         fi
         # get customer_id
         CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
+
+        # insert bike rental
+        INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id, bike_id) VALUES('$CUSTOMER_ID', '$BIKE_ID_TO_RENT')")
+
         
       fi
     fi
