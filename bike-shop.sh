de5fecff4e3eb2f78543808d665afa011ddd8390
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 09:08:02 +0200

    feat: get customer id, return to rent block

--- bike-shop.sh
+++ bike-shop.sh
@@ -75,6 +75,8 @@ RENT_MENU() {
         # insert new customer
         INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$PHONE_NUMBER', '$CUSTOMER_NAME')")
         fi
+        # get customer_id
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
         
       fi
     fi
