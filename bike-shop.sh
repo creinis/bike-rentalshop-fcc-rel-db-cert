b22118891dba0626ab7fd4f536df21401a51c79c
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 09:37:46 +0200

    feat: back to main menu, return bike done

--- bike-shop.sh
+++ bike-shop.sh
@@ -144,7 +144,9 @@ RETURN_MENU() {
                 # set bike availability to true
                 SET_TO_TRUE_RESULT=$($PSQL "UPDATE bikes SET available = 'true' WHERE bike_id = $BIKE_ID_TO_RETURN")
 
-                
+                # send to main menu
+                MAIN_MENU "Thank you for returning your bike."
+
                 fi
             fi
         fi
