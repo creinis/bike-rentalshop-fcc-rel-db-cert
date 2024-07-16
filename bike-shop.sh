7e832ba179d936b3f67cdadc2f512d7debfe776d
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 08:08:52 +0200

    add: fn structures, msg welcome, msg goodbye

--- bike-shop.sh
+++ bike-shop.sh
@@ -2,3 +2,16 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 
+echo -e "\n~~~~~~~~~~ Bike Rental Shop ~~~~~~~~~~\n"
+
+MAIN_MENU() {
+}
+
+RETURN_MENU() {
+}
+
+EXIT() {
+    echo -e "\nThank you for stopping in.\n"    
+}
+
+MAIN_MENU
